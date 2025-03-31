e088de8af077f8c78f6cab9756ef452f729201bd
svintus <ilya.semenuaka@gmail.com>
2025-03-31 17:00:52 +0300

    Исправление 3

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ env = os.getenv("APP_ENV", "development")  # по умолчанию development
 if env not in ("development", "production"):
     env = "development"
 # Загружем переменные окружения из .env.development или .env.production в зависимости от значения APP_ENV
-load_dotenv(f".env.{env}")
+load_dotenv(f".env.production")
 
 HOST = os.getenv("HOST", "127.0.0.1")
 PORT = int(os.getenv("PORT", 8000))
