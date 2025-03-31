b372a3c5bd8e0746a78889db0e05b026a056ae18
svintus <ilya.semenuaka@gmail.com>
2025-03-31 17:12:30 +0300

    Исправление 5

--- backend/database.py
+++ backend/database.py
@@ -6,6 +6,7 @@ from models import Test
 load_dotenv()
 
 DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+print("DATABASE_URL:", DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 
 def init_db():
